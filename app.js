ea0387e57de389909859436710bbb3581b008aca
Hassan Osama <73666211+hassanosama5@users.noreply.github.com>
2025-11-01 18:43:32 +0200

    fixing a require

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
-const bookingController = require('./routes/bookingController');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 dotenv.config({ path: './config.env' }); // Load BEFORE any app code
